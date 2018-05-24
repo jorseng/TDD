ad72abc8cf02c7fa3f264b2c57b3be755e8fa21d
jorseng <jorseng@gmail.com>
2018-05-24 15:35:23 +0800

    changed function

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title()
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_list(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
